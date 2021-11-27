d2bd1aaa1ef22dbc2e8484951609244e8b140551
choge <2502247952@qq.com>
2021-11-27 11:26:55 +0800

    后台模板

--- myadmin/views/index.py
+++ myadmin/views/index.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('欢迎进入点餐系统的后台管理!')
+    return render(request,'myadmin/index/index.html')
