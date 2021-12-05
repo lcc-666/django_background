ce0ad5f90b5e51c7581e21f8ed08544cbfe5ed99
choge <2502247952@qq.com>
2021-12-05 10:10:39 +0800

    mobile

--- mobile/urls.py
+++ mobile/urls.py
@@ -2,17 +2,17 @@ from django.urls import path
 
 from mobile.views import index,member
 urlpatterns = [
-    path('',index.index,name='moblie_index'),
+    path('',index.index,name='mobile_index'),
 
-    path('register',index.register,name='moblie_register'),
-    path('doregister',index.doRegister,name='moblie_doregister'),
-    path('shop',index.shop,name='moblie_shop'),
-    path('shop/select',index.selectShop,name='moblie_selectShop'),
-    path('orders/add',index.addOrders,name='moblie_addOrders'),
+    path('register',index.register,name='mobile_register'),
+    path('doregister',index.doRegister,name='mobile_doregister'),
+    path('shop',index.shop,name='mobile_shop'),
+    path('shop/select',index.selectShop,name='mobile_selectShop'),
+    path('orders/add',index.addOrders,name='mobile_addOrders'),
 
-    path('member',member.index,name='moblie_member_index'),
-    path('member/orders',member.orders,name='moblie_member_orders'),
-    path('member/detail',member.detail,name='moblie_member_detail'),
-    path('member/logout',member.logout,name='moblie_member_logout'),
+    path('member',member.index,name='mobile_member_index'),
+    path('member/orders',member.orders,name='mobile_member_orders'),
+    path('member/detail',member.detail,name='mobile_member_detail'),
+    path('member/logout',member.logout,name='mobile_member_logout'),
 
 ]
